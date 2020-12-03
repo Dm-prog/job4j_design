2f6768f2de155780eab0a0702c10f608b687bf89
Dm-prog <prostokota@list.ru>
2020-12-03 12:14:13 +0400

    5.2.1. Реализовать SimpleArray<T>[156#406311]

--- src/main/java/ru/job4j/generics/SimpleArray.java
+++ src/main/java/ru/job4j/generics/SimpleArray.java
@@ -5,15 +5,22 @@ import java.util.Objects;
 
 public class SimpleArray<T> implements Iterable<T> {
     Object[] values = new Object[5];
-    Object[] copyValues = new Object[values.length - 1];
     int count = 0;
 
     @Override
     public Iterator<T> iterator() {
-        for (int i = 0; i < values.length; i++) {
 
-        }
-        return null;
+        return new Iterator<T>() {
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public T next() {
+                return null;
+            }
+        };
     }
 
     public void add(T model) {
@@ -21,24 +28,20 @@ public class SimpleArray<T> implements Iterable<T> {
     }
 
     public void set(int index, T model) {
-        for (int i = 0; i < values.length; i++) {
-            if (Objects.checkIndex(index, values.length) < values.length) {
-                values[index] = model;
-            }
+        if (Objects.checkIndex(index, values.length) <= values.length) {
+            values[index] = model;
         }
     }
 
     public void remove(int index) {
-        for (int i = index; i < values.length; i++) {
-            if (Objects.checkIndex(index, values.length) < values.length) {
-                if (i == index) {
-                    values[i] = null;
-                    for (int k = i + 1; k < values.length; k++) {
-                        values[k - 1] = values[k];
-                    }
-                    values[values.length - 1] = null;
-                    break;
+        Object[] copyValues = new Object[values.length - 1];
+        if (Objects.checkIndex(index, values.length) <= values.length) {
+            if (count == index) {
+                values[count] = null;
+                for (int k = count + 1; k < values.length; k++) {
+                    values[k - 1] = values[k];
                 }
+                values[values.length - 1] = null;
             }
         }
         System.arraycopy(values, 0, copyValues, 0, values.length - 1);
@@ -46,12 +49,7 @@ public class SimpleArray<T> implements Iterable<T> {
     }
 
     public int get(int index) {
-        for (int i = 0; i < values.length; i++) {
-            if (i == index) {
-                return index;
-            }
-        }
-        return 0;
+        return index;
     }
 }
 
