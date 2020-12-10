425ac52baf14ab42514558b92895c28ffc7abfcd
Dm-prog <prostokota@list.ru>
2020-12-10 18:58:24 +0400

    2. Создать контейнер на базе связанного списка [159#406291]

---
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+public class SimpleQueue<T> {
+    private final SimpleStack<T> in = new SimpleStack<>();
+    private final SimpleStack<T> out = new SimpleStack<>();
+
+    public T poll() {
+        push(in.pop());
+        out.push(in.pop());
+        return out.pop();
+    }
+
+    public void push(T value) {
+        in.push(value);
+    }
+}
