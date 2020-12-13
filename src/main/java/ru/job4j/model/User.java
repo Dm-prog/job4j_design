504aff665de7935f6543a6759d22fca697896458
Dm-prog <prostokota@list.ru>
2020-12-13 15:15:38 +0400

    2. Без переопределения equals и hashCode[1005#406322]

--- src/main/java/ru/job4j/model/User.java
+++ src/main/java/ru/job4j/model/User.java
@@ -16,7 +16,8 @@ public class User {
         Map<User, Object> map = new HashMap<>();
         map.put(user1, new Object());
         map.put(user2, new Object());
-        System.out.println(map);
+        System.out.println(user1.hashCode());
+        System.out.println(user2.hashCode());
     }
 
     public User(String name, int children, Calendar birthday) {
