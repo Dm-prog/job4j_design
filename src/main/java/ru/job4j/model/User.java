0dbed29e4fdc60be32805e751db710bfbfef5de9
Dm-prog <prostokota@list.ru>
2020-12-13 14:46:27 +0400

    2. Без переопределения equals и hashCode[1005#406322]

--- src/main/java/ru/job4j/model/User.java
+++ src/main/java/ru/job4j/model/User.java
@@ -1,9 +1,36 @@
 package ru.job4j.model;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class User {
     private String name;
     private int children;
     private Calendar birthday;
+
+    public static void main(String[] args) {
+        Calendar calendar = new GregorianCalendar(2000,Calendar.JANUARY,15);
+
+        User user1 = new User("Иван", 5, calendar);
+        User user2 = new User("Иван", 5, calendar);
+
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        System.out.println(map);
+    }
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", children=" + children +
+                ", birthday=" + birthday +
+                '}';
+    }
 }
