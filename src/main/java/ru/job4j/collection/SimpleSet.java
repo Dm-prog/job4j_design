1054a957dc0c039b12d819fc691b55d3a7774c01
Dm-prog <prostokota@list.ru>
2020-12-13 13:15:08 +0400

    1. Реализовать коллекцию Set на массиве[996#406315]

--- src/main/java/ru/job4j/collection/SimpleSet.java
+++ src/main/java/ru/job4j/collection/SimpleSet.java
@@ -6,8 +6,20 @@ public class SimpleSet<E> implements Iterable<E> {
     private SimpleArray<E> simpleArray = new SimpleArray<>();
 
     public void add(E e) {
-        simpleArray.add(e);
+        if (!contains(e)) {
+            simpleArray.add(e);
+        }
+    }
 
+    public boolean contains(E value) {
+        boolean rsl = false;
+        for (E e : simpleArray) {
+            if (value.equals(e)) {
+                rsl = true;
+                break;
+            }
+        }
+        return rsl;
     }
 
     @Override
