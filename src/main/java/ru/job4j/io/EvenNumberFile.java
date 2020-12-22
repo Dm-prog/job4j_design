da3567290bcc9953704b6faf5d422a9964624903
Dm-prog <prostokota@list.ru>
2020-12-22 16:54:07 +0400

    0.2. FileInputStream[4898#406348]

---
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -0,0 +1,24 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        int[] array = null;
+        try (BufferedReader in = new BufferedReader(new FileReader("even.txt"))) {
+            array = in.lines().mapToInt(Integer::parseInt).toArray();
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+        } if (array != null) {
+            for (int j : array) {
+                if (j % 2 == 0) {
+                    System.out.println(j + " - четное");
+                } else {
+                    System.out.println(j + " - нечетное");
+                }
+            }
+        }
+    }
+}
