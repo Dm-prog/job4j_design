fd085e7ef9d313725dd64105592cc7b795637c8a
Dm-prog <prostokota@list.ru>
2020-12-23 17:03:11 +0400

    0.3. BufferedReader.[252489#406339]

---
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -0,0 +1,32 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LogFilter {
+    public static List<String> filter(String file) {
+        List<String> result = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] words = line.split(" ");
+                for (String str : words) {
+                    if (str.contains("404")) {
+                        result.add(line);
+                    }
+                }
+            }
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<String> log = filter("log.txt");
+        System.out.println(log);
+    }
+}
