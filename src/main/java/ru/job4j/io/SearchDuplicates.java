102afd8e2c0b8db7c7d909343bc7da7e84bec3ea
Dm-prog <prostokota@list.ru>
2021-01-07 15:48:03 +0300

    4.2. Поиск дубликатов[315066#406343]

--- src/main/java/ru/job4j/io/SearchDuplicates.java
+++ src/main/java/ru/job4j/io/SearchDuplicates.java
@@ -4,12 +4,13 @@ import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.List;
 import java.util.Set;
 import java.util.function.Predicate;
 
-public class SearchDuplicates implements FileVisitor<Path> {
+public class SearchDuplicates extends SimpleFileVisitor<Path> {
     private Predicate<Path> predicate;
     private List<Path> paths;
     private Set<Path> set;
@@ -25,11 +26,6 @@ public class SearchDuplicates implements FileVisitor<Path> {
     }
 
     @Override
-    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
-        return FileVisitResult.CONTINUE;
-    }
-
-    @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
         if (predicate.test(file)) {
             paths.add(file);
@@ -39,16 +35,6 @@ public class SearchDuplicates implements FileVisitor<Path> {
                 set.add(file);
             }
         }
-        return FileVisitResult.CONTINUE;
-    }
-
-    @Override
-    public FileVisitResult visitFileFailed(Path file, IOException exc) {
-        return FileVisitResult.CONTINUE;
-    }
-
-    @Override
-    public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
         return FileVisitResult.CONTINUE;
     }
 }
