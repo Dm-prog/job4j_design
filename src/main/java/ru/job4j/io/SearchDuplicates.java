a3114f8de70ae1200ec1b718987c57be90d0f88a
Dm-prog <prostokota@list.ru>
2021-01-08 17:05:01 +0300

    4.2. Поиск дубликатов[315066#406343]

--- src/main/java/ru/job4j/io/SearchDuplicates.java
+++ src/main/java/ru/job4j/io/SearchDuplicates.java
@@ -1,15 +1,11 @@
 package ru.job4j.io;
 
-import java.io.IOException;
 import java.nio.file.FileVisitResult;
-import java.nio.file.FileVisitor;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-import java.util.function.Predicate;
 
 public class SearchDuplicates extends SimpleFileVisitor<Path> {
     private final List<FileProperty> dublicates;
@@ -23,14 +19,12 @@ public class SearchDuplicates extends SimpleFileVisitor<Path> {
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-        for (FileProperty property : dublicates) {
-            FileProperty fileProperty = new FileProperty();
-            if (property.equals(fileProperty)) {
-                dublicates.add(fileProperty);
-            } else {
-                set.add(fileProperty);
-            }
-        }
+//        FileProperty fileProperty = file.;
+//        if (!set.contains(fileProperty)) {
+//            set.add(fileProperty);
+//        } else {
+//            dublicates.add(fileProperty);
+//        }
         return FileVisitResult.CONTINUE;
     }
 }
