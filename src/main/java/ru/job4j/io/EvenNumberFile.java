a227b13b0362a43de8344339c60ec6164dd15aa3
Dm-prog <prostokota@list.ru>
2021-01-09 12:03:59 +0300

    commit

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -15,7 +15,6 @@ public class EvenNumberFile {
                     System.out.println(str + " - нечетное");
                 }
             }
-
         } catch (IOException | NumberFormatException e) {
             e.printStackTrace();
         }
