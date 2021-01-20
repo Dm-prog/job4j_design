7b35fbbf78fc68b7cbf81da2f9176b729be6439d
Dm-prog <prostokota@list.ru>
2021-01-20 16:45:08 +0300

    Поиск файлов по критерию[783#406357]

--- src/main/java/ru/job4j/io/SearchFilesByCriteria.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.io;
-
-public class SearchFilesByCriteria {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/ru/job4j/io/searcher/Args.java
@@ -0,0 +1,20 @@
+package ru.job4j.io.searcher;
+
+public class Args {
+
+    public String directory() {
+        return "d";
+    }
+
+    public String fileName() {
+        return "n";
+    }
+
+    public String mask() {
+        return "m";
+    }
+
+    public String result() {
+        return "o";
+    }
+}
