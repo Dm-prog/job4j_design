9dafbf730b397b0107b0e10c9bfc35060d6a5f9a
Dm-prog <prostokota@list.ru>
2021-01-20 21:03:52 +0300

    Поиск файлов по критерию[783#406357]

--- src/main/java/ru/job4j/io/searcher/Args.java
+++ src/main/java/ru/job4j/io/searcher/Args.java
@@ -1,6 +1,31 @@
 package ru.job4j.io.searcher;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Args {
+    private Map<String, String> keys = new HashMap<>();
+    private final String[] args;
+
+    public static void main(String[] args) {
+
+    }
+
+    public Args(String[] args) {
+        this.args = args;
+        validate();
+    }
+
+    private boolean validate() {
+        for (String arg : args) {
+            String[] line = arg.split("=");
+            if (line.length != 2) {
+                throw new IllegalArgumentException();
+            }
+            keys.put(line[0].replaceFirst("-", ""), line[1]);
+        }
+        return true;
+    }
 
     public String directory() {
         return "d";
