b4a760beebde5ecbbf171d27390255e816ceb883
Dm-prog <prostokota@list.ru>
2021-02-04 21:18:23 +0300

    4.5. Travis CI и PostgreSQL[98950#406241]

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,51 +1,38 @@
 package ru.job4j.io;
 
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
-import java.io.*;
-import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 
-import static org.junit.Assert.*;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class AnalizyTest {
-    Analizy analizy;
 
-    @Before
-    public void analizy() {
-        analizy = new Analizy();
-    }
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
-    public void unavailable() throws IOException {
-        TemporaryFolder folder = new TemporaryFolder();
-        File fileLog = folder.newFile("server.log");
-        File fileCsv = folder.newFile("unavailable.csv");
-        List<String> list = new ArrayList<>();
-        try (PrintWriter pw = new PrintWriter(new FileOutputStream(fileLog))) {
-            pw.println("200 10:56:01");
-            pw.println("200 10:57:01");
-            pw.println("400 10:58:01");
-            pw.println("200 10:59:01");
-            pw.println("500 11:01:02");
-            pw.println("200 11:02:02");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        analizy.unavailable("server.log", "unavailable.csv");
-
-        try (BufferedReader br = new BufferedReader(new FileReader(fileCsv))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                list.add(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        List<String> exp = List.of("10:58:01;10:59:01;", "11:01:02;11:02:02;");
-        assertThat(list, is(exp));
+    public void simpleTest() throws IOException {
+        File src = folder.newFile("unavailable.csv");
+        File tgt = folder.newFile("target.csv");
+        Files.write(src.toPath(), List.of(
+                "200 10:56:01",
+                "500 10:57:01",
+                "400 10:58:01",
+                "200 10:59:01",
+                "500 11:01:02",
+                "200 11:02:02"
+        ));
+        Analizy analizy = new Analizy();
+        analizy.unavailable(src.getAbsolutePath(), tgt.getAbsolutePath());
+        List<String> expect = List.of("10:57:01;10:59:01;", "11:01:02;11:02:02;");
+        List<String> result = Files.readAllLines(tgt.toPath());
+        assertThat(result, is(expect));
     }
 }
