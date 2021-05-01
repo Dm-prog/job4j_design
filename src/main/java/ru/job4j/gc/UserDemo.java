4bd1fa54dbfed9ae62bbaa4a97b3cba70dc72d2b
Dm-prog <prostokota@list.ru>
2021-05-01 12:00:57 +0300

    Демонстрация работы GC[1589#406391]

--- src/main/java/ru/job4j/gc/UserDemo.java
+++ src/main/java/ru/job4j/gc/UserDemo.java
@@ -1,5 +1,7 @@
 package ru.job4j.gc;
 
+import static com.carrotsearch.sizeof.RamUsageEstimator.sizeOf;
+
 public class UserDemo {
     private static final long KB = 1000;
     private static final long MB = KB * KB;
@@ -16,11 +18,13 @@ public class UserDemo {
     }
 
     public static void main(String[] args) {
+        User user = new User(16, "name");
+        System.out.println(sizeOf(user));
         info();
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 8000; i++) {
             new User(i, "name_" + i);
         }
-        System.gc();
+        //System.gc();
         info();
     }
 }
