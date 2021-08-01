a4a2d43fafe14d506ce0042e5c882fdbd533625f
Dm-prog <prostokota@list.ru>
2021-08-01 19:05:53 +0300

    2. Парковка машин [#853]

---
+++ src/main/java/ru/job4j/parking/CalculateParkingSpace.java
@@ -0,0 +1,8 @@
+package ru.job4j.parking;
+
+public class CalculateParkingSpace implements ParkingSpaces {
+    @Override
+    public int parking(int numberOfPlaces) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/job4j/parking/Truck.java
@@ -0,0 +1,22 @@
+package ru.job4j.parking;
+
+public class Truck {
+    private int carNumber;
+    private int carName;
+
+    public int getCarNumber() {
+        return carNumber;
+    }
+
+    public void setCarNumber(int carNumber) {
+        this.carNumber = carNumber;
+    }
+
+    public int getCarName() {
+        return carName;
+    }
+
+    public void setCarName(int carName) {
+        this.carName = carName;
+    }
+}
