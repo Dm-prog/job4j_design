75e5a50dddb2c1a910853a37a46da8dc5d14af04
Dm-prog <prostokota@list.ru>
2021-08-27 15:15:54 +0300

    1. Создать меню. [#4748 #189416]

--- src/main/java/ru/job4j/menu/Menu.java
+++ src/main/java/ru/job4j/menu/Menu.java
@@ -2,30 +2,25 @@ package ru.job4j.menu;
 
 import java.util.*;
 
-public class Menu implements Action {
+public class Menu {
 
     private Item item;
 
-    @Override
-    public void act() {
-
-    }
-
     // добавляет потомка к предку. Служит чтоб сконструировать меню
     public void add(Item parentName, Item child, Action action) {
-        Optional<Item> parentItem = find(parentName);
-        Optional<Item> childrenNode = find(child);
+        Optional<Item> parentItem = find(parentName.getName());
+        Optional<Item> childrenNode = find(child.getName());
         if(parentItem.isEmpty() || childrenNode.isPresent()) {
             parentItem.get().getItems().add(new Item(item.getName(), action, item.getItems()));
         }
     }
 
-    private Optional<Item> find(Item key) {
+    private Optional<Item> find(String name) {
         Optional<Item> found = Optional.empty();
         Queue<Item> queue = new LinkedList<>(List.of(item));
         while (!queue.isEmpty()) {
             Item el = queue.poll();
-            if (Objects.equals(el, key)) {
+            if (Objects.equals(el, name)) {
                 found = Optional.of(el);
                 break;
             }
@@ -36,16 +31,15 @@ public class Menu implements Action {
 
     // получает пункт по имени. Уже из него можно вытащить действие, которое можно будет вызвать
     public Item get(String name) {
-        for (Item i : item.getItems()) {
-            if (i.getName().equals(name)) {
-                return i;
-            }
-        }
-        return null;
+        Optional<Item> optional = find(name);
+        return optional.orElse(null);
     }
 
     // возвращает строковое представление меню
-    public String print() {
-        return null;
+    public String print(int lvl, StringBuilder out) {
+        String prefix = "-".repeat(lvl);
+        out.append(String.format("%sn", (prefix + " ").stripLeading()));
+        item.getItems().forEach(c -> print(lvl + 1, out));
+        return out.toString();
     }
 }
